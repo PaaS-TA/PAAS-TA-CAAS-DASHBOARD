41787d74a07f8aa3e07ab0d25c80416fcd16360e
rex <rexx4314@gmail.com>
2018-08-07 13:11:23 +0900

    modify sample rest template service, add Namespace model, jsp page

--- src/main/java/org/paasta/caas/dashboard/cluster/namespaces/NamespaceService.java
+++ src/main/java/org/paasta/caas/dashboard/cluster/namespaces/NamespaceService.java
@@ -1,20 +1,14 @@
 package org.paasta.caas.dashboard.cluster.namespaces;
 
+import org.paasta.caas.dashboard.common.Constants;
+import org.paasta.caas.dashboard.common.RestTemplateService;
 import org.paasta.caas.dashboard.config.EnvConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.HashMap;
-import java.util.Map;
 
 //import org.apache.http.client.HttpClient;
 //import javax.servlet.http.HttpServletResponse;
@@ -31,47 +25,58 @@ import java.util.Map;
 public class NamespaceService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NamespaceService.class);
+    private static final String REQ_URL = "/cluster/namespaces";
 
+    // TODO :: REMOVE
     @Autowired
     RestTemplate restTemplate;
 
+    // TODO :: REMOVE
     @Autowired
     private EnvConfig envConfig;
+
+    private final RestTemplateService restTemplateService;
+
+    @Autowired
+    public NamespaceService(RestTemplateService restTemplateService) {this.restTemplateService = restTemplateService;}
 
     /**
      * 네임스페이스 리스트 조회
      *
      * @return int int
      */
-    public Map<String, Object> getNamespaceList(Map<String, Object> map) {
-        Map result = new HashMap();
+    // TODO :: REMOVE
+//    public Map<String, Object> getNamespaceList(Map<String, Object> map) {
+//        Map result = new HashMap();
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        //headers.add("Authorization", "Bearer "+kubeAccountToken);
+//        //headers.add("Content-Type", "application/json");
+//
+//        Map<String, Object> param = new HashMap<>();
+//        String kubeAccountToken = "";
+//        if (!ObjectUtils.isEmpty(map.get("token"))) {
+//            kubeAccountToken = map.get("token").toString();
+//            LOGGER.info("Get InputToken : "+ kubeAccountToken);
+//        }
+//        //URI Builder
+//        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(envConfig.getCaasApiUrl()+"/cluster/namespaces")
+//                .queryParam("token", kubeAccountToken);
+//
+//        HttpEntity<Map> resetEntity = new HttpEntity(headers);
+//        ResponseEntity<Map> responseEntity = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, resetEntity, Map.class);
+//
+//        LOGGER.debug(responseEntity.getBody().toString());
+//
+//        result.put("result", "success");
+//        result.put("data", responseEntity.getBody());
+//        result.put("statusCode", responseEntity.getStatusCodeValue());
+//
+//        return result;
+//    }
 
-        HttpHeaders headers = new HttpHeaders();
-        //headers.add("Authorization", "Bearer "+kubeAccountToken);
-        //headers.add("Content-Type", "application/json");
-
-        Map<String, Object> param = new HashMap<>();
-        String kubeAccountToken = "";
-        if (!ObjectUtils.isEmpty(map.get("token"))) {
-            kubeAccountToken = map.get("token").toString();
-            LOGGER.info("Get InputToken : "+ kubeAccountToken);
-        }
-        //URI Builder
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(envConfig.getCaasApiUrl()+"/cluster/namespaces")
-                .queryParam("token", kubeAccountToken);
-
-        HttpEntity<Map> resetEntity = new HttpEntity(headers);
-        ResponseEntity<Map> responseEntity = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, resetEntity, Map.class);
-
-        LOGGER.debug(responseEntity.getBody().toString());
-
-        result.put("result", "success");
-        result.put("data", responseEntity.getBody());
-        result.put("statusCode", responseEntity.getStatusCodeValue());
-
-        return result;
+    Namespace getNamespaceList() {
+        return restTemplateService.send(Constants.TARGET_CAAS_API, REQ_URL, HttpMethod.GET, null, Namespace.class);
     }
-
-
 
 }
--- src/main/java/org/paasta/caas/dashboard/common/RestTemplateService.java
+++ src/main/java/org/paasta/caas/dashboard/common/RestTemplateService.java
@@ -1,24 +1,15 @@
 package org.paasta.caas.dashboard.common;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Base64Utils;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
-import java.util.Objects;
 
 /**
  * Rest Template Service 클래스
@@ -75,33 +66,6 @@ public class RestTemplateService {
 
 
     /**
-     * Send map.
-     *
-     * @param reqApi     the req api
-     * @param reqUrl     the req url
-     * @param httpMethod the http method
-     * @param bodyObject the body object
-     * @return the map
-     */
-    public Map send(String reqApi, String reqUrl, HttpMethod httpMethod, Object bodyObject) {
-
-        setApiUrlAuthorization(reqApi);
-
-        HttpHeaders reqHeaders = new HttpHeaders();
-        reqHeaders.add(AUTHORIZATION_HEADER_KEY, base64Authorization);
-        reqHeaders.add(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-
-        HttpEntity<Object> reqEntity = new HttpEntity<>(bodyObject, reqHeaders);
-
-        LOGGER.info("POST >> Request: {}, {baseUrl} : {}, Content-Type: {}", HttpMethod.POST, reqUrl, reqHeaders.get(CONTENT_TYPE));
-        ResponseEntity<Map> resEntity = restTemplate.exchange(baseUrl + reqUrl, httpMethod, reqEntity, Map.class);
-        LOGGER.info("Map send :: Response Type: {}", Objects.requireNonNull(resEntity.getBody()).getClass());
-
-        return resEntity.getBody();
-    }
-
-
-    /**
      * Send t.
      *
      * @param <T>          the type parameter
@@ -134,82 +98,6 @@ public class RestTemplateService {
         return resEntity.getBody();
     }
 
-
-    /**
-     * Cf send t.
-     *
-     * @param <T>          the type parameter
-     * @param reqToken     the req token
-     * @param reqUrl       the req url
-     * @param httpMethod   the http method
-     * @param bodyObject   the body object
-     * @param responseType the response type
-     * @return the t
-     */
-    public <T> T cfSend(String reqToken, String reqUrl, HttpMethod httpMethod, Object bodyObject, Class<T> responseType) {
-
-        HttpHeaders reqHeaders = new HttpHeaders();
-        reqHeaders.add(AUTHORIZATION_HEADER_KEY, "bearer " + reqToken);
-        reqHeaders.add(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-
-        HttpEntity<Object> reqEntity = new HttpEntity<>(bodyObject, reqHeaders);
-
-        LOGGER.info("<T> T send :: Request : {} {baseUrl} : {}, Content-Type: {}", httpMethod, reqUrl, reqHeaders.get(CONTENT_TYPE));
-        ResponseEntity<T> resEntity = restTemplate.exchange(reqUrl, httpMethod, reqEntity, responseType);
-        if (resEntity.getBody() != null) {
-            LOGGER.info("Response Type: {}", resEntity.getBody().getClass());
-        } else {
-            LOGGER.info("Response Type: {}", "response body is null");
-        }
-
-
-        return resEntity.getBody();
-    }
-
-    /**
-     * Send multipart t.
-     *
-     * @param <T>          the type parameter
-     * @param reqApi       the req api
-     * @param reqUrl       the req url
-     * @param file         the file
-     * @param responseType the response type
-     * @return the t
-     * @throws Exception the exception
-     */
-    public <T> T sendMultipart(String reqApi, String reqUrl, MultipartFile file, Class<T> responseType) throws Exception {
-
-        setApiUrlAuthorization(reqApi);
-
-        HttpHeaders reqHeaders = new HttpHeaders();
-        reqHeaders.add(AUTHORIZATION_HEADER_KEY, base64Authorization);
-        reqHeaders.add(CONTENT_TYPE, "multipart/form-data");
-
-        final MultiValueMap<String, Object> data = new LinkedMultiValueMap<>();
-
-        ByteArrayResource resource = new ByteArrayResource(file.getBytes()) {
-            @Override
-            public String getFilename() throws IllegalStateException {
-                return file.getOriginalFilename();
-            }
-        };
-
-        data.add("file", resource);
-        data.add("fileName", file.getOriginalFilename());
-        final HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(data, reqHeaders);
-
-        LOGGER.info("POST >> Request: {} {baseUrl} : {}, Content-Type: {}", HttpMethod.POST, reqUrl, reqHeaders.get(CONTENT_TYPE));
-        final ResponseEntity<T> resEntity = restTemplate.exchange(baseUrl + reqUrl, HttpMethod.POST, requestEntity, responseType);
-        if (resEntity.getBody() != null) {
-            LOGGER.info("Map sendMultipart :: Response Type: {}", reqUrl, resEntity.getBody().getClass());
-        } else {
-            LOGGER.info("Response Type: {}", "response body is null");
-        }
-
-        return resEntity.getBody();
-    }
-
-
     private void setApiUrlAuthorization(String reqApi) {
 
         String apiUrl = "";
@@ -229,35 +117,5 @@ public class RestTemplateService {
 
         this.base64Authorization = authorization;
         this.baseUrl = apiUrl;
-    }
-
-    /**
-     * Make query param string.
-     *
-     * @param reqUrl      the req url
-     * @param obj         the obj
-     * @param exceptParam the except param
-     * @return the string
-     */
-    public String makeQueryParam(String reqUrl, Object obj, String exceptParam) {
-
-        ObjectMapper om = new ObjectMapper();
-
-        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(reqUrl);
-        Map<String, Object> codingRulesMap = om.convertValue(obj, Map.class);
-        StringBuilder exceptQuery = new StringBuilder();
-
-        for (String key : codingRulesMap.keySet()) {
-
-            if (codingRulesMap.get(key) != null && !key.equals(exceptParam)) {
-                builder.queryParam(key, codingRulesMap.get(key));
-            } else if (key.equals(exceptParam)) {
-                exceptQuery.append(exceptParam).append("=").append(codingRulesMap.get(key));
-            }
-        }
-
-        String queryParam = builder.build().encode().toUriString();
-
-        return StringUtils.isEmpty(queryParam) ? "?" + exceptQuery : queryParam + "&" + exceptQuery;
     }
 }
