9cd0e6c69d902a13c9c4e8a8297c04f689b884e3
rex <rexx4314@gmail.com>
2018-08-09 13:01:15 +0900

    apply web mvc config with bean, remove mvc view config in application config

---
+++ src/main/java/org/paasta/caas/dashboard/config/WebMvcConfig.java
@@ -0,0 +1,41 @@
+package org.paasta.caas.dashboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Web Mvc Config 클래스
+ *
+ * @author REX
+ * @version 1.0
+ * @since 2018.08.08
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    /**
+     * View resolver view resolver.
+     *
+     * @return the view resolver
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
