c2f6f1cf2d8720bc2c48ee77195e961c8119cec5
hrjin <hyunlee@bluedigm.com>
2018-10-01 20:09:43 +0900

    안 쓰는 import 삭제

--- src/main/java/org/paasta/caas/dashboard/roles/RolesService.java
+++ src/main/java/org/paasta/caas/dashboard/roles/RolesService.java
@@ -13,7 +13,6 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.List;
 import java.util.Objects;
 
 /**
--- src/main/java/org/paasta/caas/dashboard/users/UsersService.java
+++ src/main/java/org/paasta/caas/dashboard/users/UsersService.java
@@ -18,7 +18,6 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
