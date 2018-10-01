242256c0ac11c72a91aa3965a548c90f8337894a
rex <rexx4314@gmail.com>
2018-10-01 17:07:26 +0900

    code cleanup

--- src/main/java/org/paasta/caas/dashboard/common/Constants.java
+++ src/main/java/org/paasta/caas/dashboard/common/Constants.java
@@ -80,13 +80,13 @@ public class Constants {
 
     public static final String URI_API_SECRETS_DETAIL = "/namespaces/{namespace:.+}/secrets/{accessTokenName:.+}";
 
+
     // COMMON API URI
     public static final String URI_COMMON_API_USERS_LIST = "/users/serviceInstanceId/{serviceInstanceId:.+}/organizationGuid/{organizationGuid:.+}";
     public static final String URI_COMMON_API_USERS_DETAIL = "/users/serviceInstanceId/{serviceInstanceId:.+}/organizationGuid/{organizationGuid:.+}/userId/{userId:.+}/";
     public static final String URI_COMMON_API_USERS_UPDATE = "/users/serviceInstanceId/{serviceInstanceId:.+}/organizationGuid/{organizationGuid:.+}/userId/{userId:.+}/update";
     public static final String URI_COMMON_API_USERS_DELETE = "/users/serviceInstanceId/{serviceInstanceId:.+}/organizationGuid/{organizationGuid:.+}/userId/{userId:.+}/delete";
 
-    // COMMON API URI :: ROLES
     public static final String URI_COMMON_API_ROLES_LIST = "/roles/{roleSetCode:.+}";
 
     private Constants() {
