406ee76a9d857a7a1aa5242b208aefafd58c5ad8
CissC2 <d3g3nyo@gmail.com>
2018-10-31 18:40:36 +0900

    초기 진입 및 매 호마다 네임스페이스 체크 관련 인터셉터 수정

--- src/main/java/org/paasta/caas/dashboard/config/security/CustomInterceptor.java
+++ src/main/java/org/paasta/caas/dashboard/config/security/CustomInterceptor.java
@@ -9,7 +9,6 @@ import org.paasta.caas.dashboard.users.UsersList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,7 +17,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -41,30 +39,37 @@ public class CustomInterceptor extends HandlerInterceptorAdapter {
     @Autowired
     CustomUserDetailsService customUserDetailsService;
 
-
+    /**
+     * preHandle 구현
+     * 최초 접속시 redirect 및 namespace 체크
+     *
+     * @param request    HttpServletRequest
+     * @param response   HttpServletResponse
+     * @param handler    Object
+     * @return boolean
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String url = request.getRequestURI();
-        String ajaxHeader = request.getHeader("X-CaaS-Ajax-call"); // Ajax Call header info
+        String ajaxHeader = request.getHeader("X-CaaS-Ajax-call"); // Ajax Call header value
 
-        LOGGER.info("### Intercepter start ###");
+        LOGGER.info("### Interceptor start ###");
         LOGGER.info("** Request URI - "+url);
 
         try {
             if (!url.contains("/common/error/unauthorized") && !url.contains("/resources")) {
-                Pattern pattern = Pattern.compile("("+Constants.CAAS_INIT_URI+"/)([a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})");
-                Matcher matcher = pattern.matcher(url);
 
                 Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
                 SsoAuthenticationDetails ssoAuthenticationDetails = null;
                 String serviceInstanceId = "";
-
-                boolean isUnauthorized = false; // author 참조 에러 : true  정상 : false
+                Pattern pattern = Pattern.compile("("+Constants.CAAS_INIT_URI+"/)([a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})");
+                Matcher matcher = pattern.matcher(url);
                 boolean isInitUrl      = url.contains(Constants.CAAS_INIT_URI + "/") && matcher.find(); // 초기화 URL 여부
+                boolean isUnauthorized = false; // author 참조 에러 : true  정상 : false
 
                 try {
 
-                    // Default. SSO 정보 체크
+                    // Default. 권한 정보 체크
                     if (authentication != null) {
                         ssoAuthenticationDetails = (SsoAuthenticationDetails) authentication.getDetails();
                     } else { // authentication == null
@@ -75,7 +80,7 @@ public class CustomInterceptor extends HandlerInterceptorAdapter {
                     // A. 인스턴스 ID를 가지고 최초 진입 페이지 접근시, 권한 참고 오류 처리
                     if(isInitUrl){
 
-                        // A-1. SSO 정보 정상일시 처리
+                        // A-1. 권한 정보 정상일시 처리
                         if(!isUnauthorized){
 
                             serviceInstanceId = request.getServletPath().split("/")[4];
